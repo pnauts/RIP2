d5941d48abce0d8b911e93a7ef48d367acc04e34
jorgenunezrivera <jorge.nunez.rivera@udc.es>
2016-03-21 15:23:53 +0100

    lo que hice el otro dia (no me acuerdo) termswithdocfres y docswithtermfreqs

--- P2/src/main/java/RI/P2/IndexInspectorJorge.java
+++ P2/src/main/java/RI/P2/IndexInspectorJorge.java
@@ -12,19 +12,27 @@ import java.util.Map;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.AtomicReader;
+import org.apache.lucene.index.CompositeReader;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.DocsEnum;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.SingleTermsEnum;
+import org.apache.lucene.index.SlowCompositeReaderWrapper;
+import org.apache.lucene.index.Fields ;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
@@ -55,6 +63,9 @@ public class IndexInspectorJorge {
 		List<String> fields = new ArrayList<String>();
 		List<String> stqueries = new ArrayList<String>();
 		Path outdir = null;
+		int minindex=0 , maxindex=0;
+		boolean termsdf=false,docstf=false,docstf2=false;
+		Term term = null;
 
 		for (int i = 0; i < args.length; i++) {
 			if ("-index".equals(args[i])) {
@@ -74,11 +85,34 @@ public class IndexInspectorJorge {
 					stqueries.add(args[i + 1]);
 					i ++;
 				}
+			}else if ("-termswithdocFreq".equals(args[i])) {
+				fields.add(args[i+1]);
+				minindex=Integer.parseInt(args[i+2]);
+				maxindex=Integer.parseInt(args[i+3]);
+				termsdf=true;
+				i+=3;
+			}else if ("-docswithtermFreq".equals(args[i])) {
+				fields.add(args[i+1]);
+				minindex=Integer.parseInt(args[i+2]);
+				maxindex=Integer.parseInt(args[i+3]);
+				docstf=true;
+				i+=3;
+			}else if ("-docswithtermFreq2".equals(args[i])) {
+				String field = args[i+2];
+				term=new Term(field,args[i+1]);
+				minindex=Integer.parseInt(args[i+3]);
+				maxindex=Integer.parseInt(args[i+4]);
+				docstf2=true;
+				fields.add(field);
+				i+=4;
+				
 			}
 		}
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(
 				index)));
-		IndexSearcher searcher = new IndexSearcher(reader);
+		AtomicReader atomicReader = SlowCompositeReaderWrapper.wrap((CompositeReader) reader);
+		IndexSearcher searcher = new IndexSearcher(atomicReader);
+		
 		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
 		
 		String [] auxfields = new String[fields.size()];
@@ -90,7 +124,7 @@ public class IndexInspectorJorge {
 		
 		MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_40, auxfields, analyzer);
 		
-			doPagingSearch(searcher, stqueries, outdir, fields,reader);
+			doPagingSearch(searcher, stqueries, outdir, fields,atomicReader,term,termsdf,docstf,docstf2,minindex,maxindex);
 		}
 	
 
@@ -107,17 +141,21 @@ public class IndexInspectorJorge {
 	 */
 	public static void doPagingSearch(
 			IndexSearcher searcher, List<String> queries, 
-			Path outpath, List<String> fields,IndexReader reader) throws IOException {
+			Path outpath, List<String> fields,AtomicReader reader,Term term,boolean termdf,boolean docstf,boolean docstf2,int minindex,int maxindex) throws IOException {
 
 		// Collect enough docs to show 5 pages
 		TotalHitCountCollector collector = new TotalHitCountCollector();
 		
-		BooleanQuery booleanQuery = new BooleanQuery();
-		for(int i = 0; i < fields.size(); i++){
-			Query query = new TermQuery(new Term(fields.get(i), queries.get(i)));
-			booleanQuery.add(query, BooleanClause.Occur.SHOULD);
+		Query booleanQuery = new BooleanQuery();
+		if(queries != null && queries.size()>0){
+			for(int i = 0; i < fields.size(); i++){
+				Query query = new TermQuery(new Term(fields.get(i), queries.get(i)));
+				((BooleanQuery) booleanQuery).add(query, BooleanClause.Occur.SHOULD);
+			}
+		}else{
+			booleanQuery=new MatchAllDocsQuery();
 		}
-		
+				
 		searcher.search(booleanQuery, collector);
 		TopDocs results = searcher.search(booleanQuery,
 				Math.max(1, collector.getTotalHits()));
@@ -143,35 +181,64 @@ public class IndexInspectorJorge {
 			Directory dir = FSDirectory.open(outpath.toFile());
 			writer = new IndexWriter(dir, iwc);
 		}
+		Fields allfields=null;
+		allfields=reader.fields();
+		Terms terms =null;
+		TermsEnum termsEnum = null;
+		if(termdf || docstf || docstf2){
+			
+			
+			Map<String,Long> tf = new HashMap();
+			Map<String,Long> df = new HashMap();
+			
+			
+			for(String field: allfields){
+				System.out.println("Field: "+ field);
+				terms=allfields.terms(field);
+				termsEnum=terms.iterator(null);
+				while(termsEnum.next()!=null){
+					String nombre=termsEnum.term().utf8ToString();
+					long totalTermFreq=termsEnum.totalTermFreq();
+					//long docFreq=termsEnum.docFreq();
+					if(termdf&&totalTermFreq>=minindex&&totalTermFreq<maxindex){
+						System.out.println(nombre+ "    " + totalTermFreq);						
+					}	
+					DocsEnum docsEnum=termsEnum.docs(null, null);
+					int docf=0;
+					while(docsEnum.nextDoc()!=DocIdSetIterator.NO_MORE_DOCS){
+						int id=docsEnum.docID();
+						long termFreq=docsEnum.freq();
+						if(docstf&&termFreq>=minindex&&termFreq<maxindex){
+							docf++;
+						}
+						//System.out.println(termsEnum.term().utf8ToString());
+						//System.out.println(term.text());
+						if(docstf2&&field.equals(fields.get(0))&&term.text().equals(termsEnum.term().utf8ToString())&&termFreq>=minindex&&termFreq<maxindex){
+							System.out.println("docid : " +Integer.toString(id)+ "  termfreq  " + termFreq);							
+						}
+					}
+					if(docf>0)
+						System.out.println(nombre + "\t\t\t" + docf);
+					
+				}
+			}	
+		}	
+		
 		for (int i = start; i < end; i++) {
 			if (outpath != null && outpath.toFile().isDirectory()) {
 				writer.addDocument(searcher.doc(hits[i].doc));
 
 			}
-			Map<String,Integer> terminos = new HashMap();
-			TermsEnum termsEnum = null;
-			Terms vector = reader.getTermVector(i , "title") ;
-			System.out.println(vector.toString());
-			termsEnum=vector.iterator(termsEnum);
-			BytesRef termino=null;
-			while((termino=termsEnum.next())!=null){
-				String nombre=termino.utf8ToString();
-				int frecuencia = (int)termsEnum.totalTermFreq();
-				terminos.put(nombre,frecuencia);				
-			}
-			System.out.println(terminos.toString());
+			
+				
+			
+			
+			
+			//  t o t a
+			//System.out.println(terminos.toString());
 			Document doc = searcher.doc(hits[i].doc);
 			String path = doc.get("path");
-			if (path != null) {
-				System.out.println((i + 1) + ". " + path);
-				String title = doc.get("title");
-				if (title != null) {
-					System.out.println("   Title: " + doc.get("title"));
-				}
-			} else {
-				System.out
-						.println((i + 1) + ". " + "No path for this document");
-			}
+			
 
 		}
 		if (writer != null)
